626fd10fb3d30b1255384187c220b13fbab3d915
leipzigj <leipzigj@036d89ee-4177-4ba0-82d8-1aa210390fb1>
2011-09-20 19:37:14 +0000

    recast update
    git-svn-id: http://versioncontrol.chop.edu/svn/cbmi/ganguly@3180 036d89ee-4177-4ba0-82d8-1aa210390fb1

--- trunk/hgRecastReport.R
+++ trunk/hgRecastReport.R
@@ -2,9 +2,9 @@ library(reshape)
 setwd("/storage/Ganguly/gangulyRBhi")
 
 #1) forward ref alleles; 2) reverse ref; 3) forward non-ref; 4) reverse non-ref alleles
-normal<-read.table("normal.hairpin.col",header=FALSE,col.names=c("miRNA","pos","ref","var","refnorm","revrefnorm","varnorm","revvarnorm"))
+normal<-read.table("normal.hg19.col",header=FALSE,col.names=c("miRNA","chr","pos","ref","var","refnorm","revrefnorm","varnorm","revvarnorm"))
 #normal$cell<-"normal"
-tumor<-read.table("tumor.hairpin.col",header=FALSE,col.names=c("miRNA","pos","ref","var","reftumor","revreftumor","vartumor","revvartumor"))
+tumor<-read.table("tumor.hg19.col",header=FALSE,col.names=c("miRNA","chr","pos","ref","var","reftumor","revreftumor","vartumor","revvartumor"))
 #tumor$cell<-"tumor"
 #all<-rbind(normal,tumor)
 
@@ -12,7 +12,7 @@ tumor<-read.table("tumor.hairpin.col",header=FALSE,col.names=c("miRNA","pos","re
 normal$var<-gsub(",.","",normal$var)
 tumor$var<-gsub(",.","",tumor$var)
 
-all<-merge(normal,tumor,by=c("miRNA","pos","ref","var"),all=TRUE)
+all<-merge(normal,tumor,by=c("miRNA","chr","pos","ref","var"),all=TRUE)
 all[is.na(all)]<-0
 
 all<-all[order(all$miRNA,all$pos),-grep('rev',names(all))]
@@ -29,8 +29,8 @@ getPval<-function(x){
 
 #res<-#ddply(.data=all,.(miRNA,pos,ref,var,refnorm,varnorm,reftumor,vartumor),summarize,foo=getPval)
 
-res<-ddply(.data=all,.(miRNA,pos,ref,var,refnorm,varnorm,reftumor,vartumor),transform,fisherTwoSidedpval=fisher.test(matrix(c(refnorm,varnorm,reftumor,vartumor),nrow=2,dimnames=list(c("ref","var"),c("norm","tumor"))))$p.val)
+res<-ddply(.data=all,.(miRNA,chr,pos,ref,var,refnorm,varnorm,reftumor,vartumor),transform,fisherTwoSidedpval=fisher.test(matrix(c(refnorm,varnorm,reftumor,vartumor),nrow=2,dimnames=list(c("ref","var"),c("norm","tumor"))))$p.val)
 #ddply(.data=all,.(miRNA,pos,ref,var,refnorm,varnorm,reftumor,vartumor),transform,fisherTwoSidedpval=fisher.test(matrix(c(1,2,3,4),nrow=2,dimnames=list(c("ref","var"),c("norm","tumor")))),alternative="two.sided")$p.val)
 
 print(res)
-write.csv(res,file="hairpin.edits.fisher.csv")
+write.csv(res,file="hg19.edits.fisher.csv")
