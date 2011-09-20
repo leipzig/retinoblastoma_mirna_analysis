be8b94f40d993557bc8a24bf724dec93568e8920
leipzigj <leipzigj@036d89ee-4177-4ba0-82d8-1aa210390fb1>
2011-09-20 19:23:43 +0000

    edits
    git-svn-id: http://versioncontrol.chop.edu/svn/cbmi/ganguly@3179 036d89ee-4177-4ba0-82d8-1aa210390fb1

---
+++ trunk/recastReport.R
@@ -0,0 +1,36 @@
+library(reshape)
+setwd("~/Documents/gangulyRBhi")
+
+#1) forward ref alleles; 2) reverse ref; 3) forward non-ref; 4) reverse non-ref alleles
+normal<-read.table("normalLets.col.txt",header=FALSE,col.names=c("miRNA","pos","ref","var","refnorm","revrefnorm","varnorm","revvarnorm"))
+#normal$cell<-"normal"
+tumor<-read.table("tumorLets.col.txt",header=FALSE,col.names=c("miRNA","pos","ref","var","reftumor","revreftumor","vartumor","revvartumor"))
+#tumor$cell<-"tumor"
+#all<-rbind(normal,tumor)
+
+#questionable
+normal$var<-gsub(",.","",normal$var)
+tumor$var<-gsub(",.","",tumor$var)
+
+all<-merge(normal,tumor,by=c("miRNA","pos","ref","var"),all=TRUE)
+all[is.na(all)]<-0
+
+all<-all[order(all$miRNA,all$pos),-grep('rev',names(all))]
+
+#melted<-melt(all,measure.vars="cell")
+#recasted<-cast(melted, miRNA+pos+ref+var+refnorm+varnorm ~ value)
+#write.csv(recasted,file="hsa-let.edits.csv")
+
+getPval<-function(x){
+  #mirnaMat<-matrix(as.numeric(x[c("refnorm","varnorm","reftumor","vartumor")]),nrow=2,dimnames=list(c("ref","var"),c("norm","tumor")))
+  mirnaMat<-matrix(as.numeric(c(x$refnorm,x$varnorm,x$reftumor,x$vartumor)),nrow=2,dimnames=list(c("ref","var"),c("norm","tumor")))
+  fisher.test(mirnaMat,alternative="two.sided")$p.val
+}
+
+#res<-#ddply(.data=all,.(miRNA,pos,ref,var,refnorm,varnorm,reftumor,vartumor),summarize,foo=getPval)
+
+res<-ddply(.data=all,.(miRNA,pos,ref,var,refnorm,varnorm,reftumor,vartumor),transform,fisherTwoSidedpval=fisher.test(matrix(c(refnorm,varnorm,reftumor,vartumor),nrow=2,dimnames=list(c("ref","var"),c("norm","tumor"))))$p.val)
+#ddply(.data=all,.(miRNA,pos,ref,var,refnorm,varnorm,reftumor,vartumor),transform,fisherTwoSidedpval=fisher.test(matrix(c(1,2,3,4),nrow=2,dimnames=list(c("ref","var"),c("norm","tumor")))),alternative="two.sided")$p.val)
+
+print(res)
+write.csv(res,file="hsa-let.edits.fisher.csv")
